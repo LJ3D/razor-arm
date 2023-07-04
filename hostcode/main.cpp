23a1a10e4f1fdf0b57723703fb9df85baf340692
Lucian James <65134690+LJ3D@users.noreply.github.com>
2023-07-04 15:21:01 +0100

    Basic full movement

--- hostcode/main.cpp
+++ hostcode/main.cpp
@@ -11,13 +11,15 @@
 std::vector<double> getJointPositions(arduinoSerial& Serial){
     char response[256];
     Serial.flush();
-    Serial.print("READ");
+    Serial.print("READ\n");
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     Serial.readBytes(response, 256);
-    std::cout << response << std::endl;
+    std::cout << "getJointPositions(): Read response: " << response << std::endl;
     double numbers[6] = {0};
     if(std::sscanf(response, "READ\n[%lf,%lf,%lf,%lf,%lf,%lf]", &numbers[0], &numbers[1], &numbers[2], &numbers[3], &numbers[4], &numbers[5]) != 6){
         std::cout << "ERR: Failed to parse response\n";
+    }else{
+        std::cout << "getJointPositions(): Parsed response: " << numbers[0] << ", " << numbers[1] << ", " << numbers[2] << ", " << numbers[3] << ", " << numbers[4] << ", " << numbers[5] << "\n";
     }
     std::vector<double> v(numbers, numbers + sizeof(numbers) / sizeof(numbers[0]));
     return v;
@@ -28,19 +30,24 @@ void setJointPositions(arduinoSerial& Serial, std::vector<double> positions){
     for(double& p : positions){
         cmd += " " + std::to_string(p);
     }
-    Serial.print(cmd);
+    std::cout << "setJointPositions(): Sending command: " << cmd << "\n";
+    Serial.print(cmd+"\n");
 }
 
 void adjustJointPos(arduinoSerial& Serial, int id, double adjustment){
+    std::cout << "\n\n====== adjustJointPos(): Adjusting joint " << id << " by " << adjustment << "\n";
     std::vector<double> currPos = getJointPositions(Serial);
     currPos[id] += adjustment;
     setJointPositions(Serial, currPos);
 }
 
 int main(){
+    // Initialise serial communication
     arduinoSerial Serial;
     Serial.openPort("/dev/ttyACM0");
     Serial.begin(B9600);
+
+    // GLFW + OpenGL boilerplate
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -61,33 +68,79 @@ int main(){
         glDebugMessageCallback(debug::messageCallback, nullptr);
     }
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // End GLFW + OpenGL boilerplate
+
     while(!glfwWindowShouldClose(window)){
-        glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
+        glfwPollEvents(); // Check for events (keyboard, mouse, etc)
 
-
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_A)){
-            Serial.print("HOME");
+        /*
+            Home the arm
+        */
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_L)){
+            Serial.print("HOME\n");
             char response[256] = {0};
             // Wait a bit for the response
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             Serial.readBytes(response, 256);
             std::cout << response << "\n";
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_F)){
+
+        /*
+            Print the current joint positions (for debugging, etc)
+        */
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_P)){
             std::vector<double> jointPositions = getJointPositions(Serial);
             for(double& j : jointPositions){
                 std::cout << j << ", ";
             }
             std::cout << "\n";
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_Z)){
+
+        /*
+            A horribly in-ergonomic way of controlling the arm :)
+        */
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_Q)){
+            adjustJointPos(Serial, 0, 15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_A)){
+            adjustJointPos(Serial, 0, -15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_W)){
+            adjustJointPos(Serial, 1, 15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_S)){
+            adjustJointPos(Serial, 1, -15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_E)){
             adjustJointPos(Serial, 2, 15.0);
         }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_D)){
+            adjustJointPos(Serial, 2, -15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_R)){
+            adjustJointPos(Serial, 3, 15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_F)){
+            adjustJointPos(Serial, 3, -15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_T)){
+            adjustJointPos(Serial, 4, 15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_G)){
+            adjustJointPos(Serial, 4, -15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_Y)){
+            adjustJointPos(Serial, 5, 15.0);
+        }
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_H)){
+            adjustJointPos(Serial, 5, -15.0);
+        }
 
-
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(window); // Render the current frame
     }
+
+    // Cleanup
     glfwTerminate();
     return 0;
 }
