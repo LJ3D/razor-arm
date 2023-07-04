d1dc5df94695eeeff615ee50b288f9aa3c80748b
GCTales <138573410+GCTales@users.noreply.github.com>
2023-07-04 10:13:46 +0100

    Add old code from USB drive

---
+++ armcode/src/RobotArmController.cpp
@@ -0,0 +1,150 @@
+#include "RobotArmController.h"
+#include <CommandParser.h>
+#include <Braccio++.h>
+#include <string.h>
+
+static float const HOME_POS[6] = {157.5, 157.5, 157.5, 157.5, 157.5, 90.0};
+
+RobotArmController *RobotArmController::Instance()
+{
+    if (instance == nullptr)
+    {
+        instance = new RobotArmController();
+    }
+    return instance;
+}
+
+bool RobotArmController::Setup()
+{
+    parser = new RobotCommandParser();
+
+    bool successful = parser->registerCommand("LIMP", "i", [](auto *args, char *response)
+                                              { RobotArmController::instance->LimpMode((args[0].asInt64 > 0)); });
+
+    successful = successful && parser->registerCommand("READ", "", [](auto *args, char *response)
+                                                       { RobotArmController::instance->Read(); });
+
+    successful = successful && parser->registerCommand("HOME", "", [](auto *args, char *response)
+                                                       { RobotArmController::instance->Home(); });
+
+    successful = successful && parser->registerCommand("SETX", "dddddd", [](auto *args, char *response)
+                                                       { RobotArmController::instance->SetCoordinates(args, response); });
+
+    successful = successful && parser->registerCommand("SET", "id", [](auto *args, char *response)
+                                                       { RobotArmController::instance->SetServo(args, response); });
+
+    successful = successful && parser->registerCommand("MOVE", "id", [](auto *args, char *response)
+                                                       { RobotArmController::instance->MoveServo(args, response); });
+
+    successful = successful && parser->registerCommand("SPEED", "i", [](auto *args, char *response)
+                                                       { RobotArmController::instance->SetSpeed(args, response); });
+    return successful;
+}
+
+void RobotArmController::Loop()
+{
+    if (Serial.available())
+    {
+        char line[128];
+        size_t lineLength = Serial.readBytesUntil('\n', line, 127);
+        line[lineLength] = '\0';
+
+        Serial.println(line);
+
+        char response[RobotCommandParser::MAX_RESPONSE_SIZE];
+        parser->processCommand(line, response);
+        Serial.println(response);
+    }
+}
+
+void RobotArmController::LimpMode(bool isLimp)
+{
+    if (isLimp)
+    {
+
+        Braccio.disengage();
+    }
+    else
+    {
+        Braccio.engage();
+    }
+}
+
+void RobotArmController::Read()
+{
+    Braccio.positions(angles);
+
+    char buffer[1000];
+    sprintf(buffer, "[%.2f,%.2f,%.2f,%.2f,%.2f,%.2f]", angles[0], angles[1], angles[2], angles[3], angles[4], angles[5]);
+    Serial.println(buffer);
+}
+
+void RobotArmController::Home()
+{
+    angles[0] = HOME_POS[0];
+    angles[1] = HOME_POS[1];
+    angles[2] = HOME_POS[2];
+    angles[3] = HOME_POS[3];
+    angles[4] = HOME_POS[4];
+    angles[5] = HOME_POS[5];
+
+    Braccio.moveTo(angles[0], angles[1], angles[2], angles[3], angles[4], angles[5]);
+    Serial.println("OK");
+}
+
+void RobotArmController::SetCoordinates(RobotCommandParser::Argument *args, char *response)
+{
+    angles[0] = (float)args[0].asDouble;
+    angles[1] = (float)args[1].asDouble;
+    angles[2] = (float)args[2].asDouble;
+    angles[3] = (float)args[3].asDouble;
+    angles[4] = (float)args[4].asDouble;
+    angles[5] = (float)args[5].asDouble;
+
+    Braccio.moveTo(angles[0], angles[1], angles[2], angles[3], angles[4], angles[5]);
+    Serial.println("OK");
+}
+
+void RobotArmController::SetServo(RobotCommandParser::Argument *args, char *response)
+{
+    int servo = args[0].asInt64;
+    float angle = (float)args[1].asDouble;
+
+    Braccio.move(servo).to(angle);
+    Serial.println("OK");
+}
+
+void RobotArmController::MoveServo(RobotCommandParser::Argument *args, char *response)
+{
+    auto servo = Braccio.get(args[0].asInt64);
+    float angleIncrement = (float)args[1].asDouble;
+
+    servo.to(servo.position() + angleIncrement);
+    Braccio.move(servo).to(angleIncrement);
+    Serial.println("OK");
+}
+
+void RobotArmController::SetSpeed(RobotCommandParser::Argument *args, char *response)
+{
+    auto speed = args[0].asInt64;
+// TODO: Test speed values
+    if (speed == 1000)
+    {
+        Braccio.setAngularVelocity(15.0);
+        Serial.println("OK");
+    }
+    else if (speed == 100)
+    {
+        Braccio.setAngularVelocity(30.0);
+        Serial.println("OK");
+    }
+    else if (speed == 10)
+    {
+        Braccio.setAngularVelocity(45.0);
+        Serial.println("OK");
+    }
+    else
+    {
+        Serial.println("Invalid speed");
+    }
+}
---
+++ armcode/src/main.cpp
@@ -0,0 +1,73 @@
+#include <Braccio++.h>
+#include "razor_robotics.c"
+#include "RobotArmController.h"
+#include <CommandParser.h>
+
+const int baudRate = 9600;
+lv_obj_t *statusLabel;
+
+RobotArmController *controller;
+
+void showBranding()
+{
+    Braccio.lvgl_lock();
+    lv_obj_t *img = lv_img_create(lv_scr_act());
+    lv_img_set_src(img, &razor_robotics);
+    lv_img_set_zoom(img, 200);
+    lv_obj_align(img, NULL, LV_ALIGN_TOP_LEFT, 0);
+
+    statusLabel = lv_label_create(lv_scr_act());
+    lv_label_set_long_mode(statusLabel, LV_LABEL_LONG_WRAP);
+    lv_label_set_recolor(statusLabel, true);
+    lv_label_set_text(statusLabel, "#FF0000 Not Connected#");
+    lv_obj_set_width(statusLabel, 150);
+    lv_obj_set_style_text_align(statusLabel, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_align(statusLabel, LV_ALIGN_CENTER, 0, -80);
+
+    Braccio.lvgl_unlock();
+}
+
+void setConnectionStatus(bool status)
+{
+    Braccio.lvgl_lock();
+    if (!status)
+    {
+        lv_label_set_text(statusLabel, "#FF0000 Not Ready#");
+    }
+    else
+    {
+        lv_label_set_text(statusLabel, "#24C718 Ready#");
+    }
+
+    Braccio.lvgl_unlock();
+}
+
+void setup()
+{
+    Serial.begin(baudRate);
+    Braccio.begin(&showBranding);
+
+    while (!Serial)
+        ;
+
+    setConnectionStatus(false);
+
+    controller = RobotArmController::Instance();
+
+    if (!controller->Setup())
+    {
+        lv_label_set_text(statusLabel, "#FF0000 Setup failed#");
+        while (1)
+        {
+            delay(1000);
+        }
+    }
+
+    Serial.println("Setup successful");
+}
+
+void loop()
+{
+    setConnectionStatus(true);
+    controller->Loop();
+}
