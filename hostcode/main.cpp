9343320ef4bf45cdbde22528fd3a3b579058707f
LJ3D <65134690+LJ3D@users.noreply.github.com>
2023-07-04 21:14:12 +0100

    Bit of cleaning up and commenting
    untested but compiles :)

--- hostcode/main.cpp
+++ hostcode/main.cpp
@@ -6,16 +6,34 @@
 #include <thread>
 #include <vector>
 
-#define DEBUG true
+#define DEBUG true // Enables opengl debug info callback
+#define RESPONSE_MAX_SIZE 256 // Guesstimated buffer size for receiving response from arm to commands
 
+
+
+/*
+    To-Do list
+    * Change code from using readBytes for responses to using readBytesUntil,
+    updating the robot arm code accordingly to add some terminator character (if it doesnt already have one).
+    Doing this should remove the lag in communications
+    * See how high the baud rate can be set. 115200 should work
+    * 
+*/
+
+
+
+/*
+    Reads all the current joint positions from the arm.
+    Currently only included as way to get potential debug info.
+*/
 std::vector<double> getJointPositions(arduinoSerial& Serial){
-    char response[256];
     Serial.flush();
     Serial.print("READ\n");
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    Serial.readBytes(response, 256);
+    char response[RESPONSE_MAX_SIZE];
+    Serial.readBytes(response, RESPONSE_MAX_SIZE);
     std::cout << "getJointPositions(): Read response: " << response << std::endl;
-    double numbers[6] = {157.5,157.5,187.5,57.5,157.5,90};
+    double numbers[6] = {157.5,157.5,187.5,57.5,157.5,90}; // Fallback to home position if sscanf fails
     if(std::sscanf(response, "READ\n[%lf,%lf,%lf,%lf,%lf,%lf]", &numbers[0], &numbers[1], &numbers[2], &numbers[3], &numbers[4], &numbers[5]) != 6){
         std::cout << "ERR: Failed to parse response\n";
     }else{
@@ -25,46 +43,92 @@ std::vector<double> getJointPositions(arduinoSerial& Serial){
     return v;
 }
 
-void adjustJointPos(arduinoSerial& Serial, int idx, double adj){
-    idx += 1; // Robot doesnt count from 0 (cringe)
-    Serial.print("MOVE " + std::to_string(idx) + " " + std::to_string(adj) + "\n");
-    char response[256] = {0};
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    Serial.readBytes(response, 256);
-    std::cout << response << "\n";
+
+/*
+    Currently unused, but useful for setting multiple joint positions in one go
+    Keeping here for now just in case it becomes useful
+*/
+void setJointPositions(arduinoSerial& Serial, std::vector<double> positions){
+    std::string cmd = "SETX";
+    for(double& p : positions){
+        cmd += " " + std::to_string(p);
+    }
+    Serial.print(cmd);
 }
 
+
+/*
+    Moves one of the joints (specified by idx, which ranges from 1-6) by adj degrees
+*/
+void adjustJointPos(arduinoSerial& Serial, int idx, double adj){
+    if(idx > 6 || idx < 1){
+        throw std::runtime_error("ERR: adjustJointPos() passed invalid idx");
+    }
+    Serial.print("MOVE " + std::to_string(idx) + " " + std::to_string(adj) + "\n");
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    char response[RESPONSE_MAX_SIZE] = {0};
+    Serial.readBytes(response, RESPONSE_MAX_SIZE);
+    std::cout << "adjustJointPos(): Read response: " << response << std::endl;
+}
+
+
+/*
+    Sets the speed of the movement of the robot arm.
+    The "stupidSpeed" parameter is named as such because currently,
+    you must pass either 1000, 100, 10, or 1 to change the arms speed.
+    It translates to:
+    * 1000 = 15 degrees per second
+    * 100 = 30 degrees per second
+    * 10 = 45 degrees per second
+    * 1 = 100 degrees per second
+    This is something that should be rewritten on the arm to make it a bit nicer to use.
+    (actually just passing the desired deg/sec instead)
+*/
 void setSpeed(arduinoSerial& Serial, int stupidSpeed){
     Serial.print("SPEED " + std::to_string(stupidSpeed) + "\n");
-    char response[256] = {0};
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    Serial.readBytes(response, 256);
-    std::cout << response << "\n";
+    char response[RESPONSE_MAX_SIZE] = {0};
+    Serial.readBytes(response, RESPONSE_MAX_SIZE);
+    std::cout << "setSpeed(): Read response: " << response << std::endl;
 }
+
+
+/*
+    Returns the arm to a "starting" position of sorts.
+    {157.5, 157.5, 187.5, 57.5, 157.5, 90}
+*/
+void homeArm(arduinoSerial& Serial){
+    Serial.print("HOME\n");
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    char response[RESPONSE_MAX_SIZE] = {0};
+    Serial.readBytes(response, RESPONSE_MAX_SIZE);
+    std::cout << "homeArm(): Read response: " << response << std::endl;
+}
+
 
 int main(){
     // Initialise serial communication
-    arduinoSerial Serial;
-    Serial.openPort("/dev/ttyACM0");
-    Serial.begin(B9600);
+    arduinoSerial Serial; // Provides very arduino-like functions for interacting with a serial device
+    Serial.openPort("/dev/ttyACM0"); // Default file for an arduino uno
+    Serial.begin(B9600); // baud rate of 9600, this can be increased (probably) (as long as robot code also updated)
 
-    // GLFW + OpenGL boilerplate
+    // Begin GLFW + OpenGL boilerplate
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Use OpenGL 3.3 core profile:
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_CORE_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    auto window = glfwCreateWindow(512, 512, "Arm controls", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(512, 512, "Arm controls", nullptr, nullptr);
     if(!window){
         throw std::runtime_error("ERR: Failed to create window");
     }
     glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height){
-        glViewport(0, 0, width, height);
+        glViewport(0, 0, width, height); // Very basic size callback
     });
     glfwMakeContextCurrent(window);
-    if(!gladLoaderLoadGL()){
+    if(!gladLoaderLoadGL()){ // Load/init GLAD
         throw std::runtime_error("ERR: Failed to load GLAD");
     }
-    if(DEBUG){
+    if(DEBUG){ // Handy debugging error messages from OpenGL
         glEnable(GL_DEBUG_OUTPUT);
         glDebugMessageCallback(debug::messageCallback, nullptr);
     }
@@ -72,6 +136,7 @@ int main(){
     // End GLFW + OpenGL boilerplate
 
     setSpeed(Serial, 10);
+    double adjustment = 15.0;
     while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT);
         glfwPollEvents(); // Check for events (keyboard, mouse, etc)
@@ -79,19 +144,14 @@ int main(){
         /*
             Home the arm
         */
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_L)){
-            Serial.print("HOME\n");
-            char response[256] = {0};
-            // Wait a bit for the response
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            Serial.readBytes(response, 256);
-            std::cout << response << "\n";
+        if(glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS){
+            
         }
 
         /*
             Print the current joint positions (for debugging, etc)
         */
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_P)){
+        if(glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS){
             std::vector<double> jointPositions = getJointPositions(Serial);
             for(double& j : jointPositions){
                 std::cout << j << ", ";
@@ -100,43 +160,43 @@ int main(){
         }
 
         /*
-            A horribly in-ergonomic way of controlling the arm :)
+            Per-joint control, a bit difficult to use but easy to implement
         */
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_R)){
-            adjustJointPos(Serial, 0, 15.0);
+        if(glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS){
+            adjustJointPos(Serial, 1, adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_F)){
-            adjustJointPos(Serial, 0, -15.0);
+        if(glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS){
+            adjustJointPos(Serial, 1, -adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_Q)){
-            adjustJointPos(Serial, 1, 15.0);
+        if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS){
+            adjustJointPos(Serial, 2, adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_E)){
-            adjustJointPos(Serial, 1, -15.0);
+        if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS){
+            adjustJointPos(Serial, 2, -adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_A)){
-            adjustJointPos(Serial, 2, 15.0);
+        if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
+            adjustJointPos(Serial, 3, adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_D)){
-            adjustJointPos(Serial, 2, -15.0);
+        if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
+            adjustJointPos(Serial, 3, -adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_W)){
-            adjustJointPos(Serial, 3, 15.0);
+        if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
+            adjustJointPos(Serial, 4, adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_S)){
-            adjustJointPos(Serial, 3, -15.0);
+        if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+            adjustJointPos(Serial, 4, -adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_UP)){
-            adjustJointPos(Serial, 4, 15.0);
+        if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS){
+            adjustJointPos(Serial, 5, adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_DOWN)){
-            adjustJointPos(Serial, 4, -15.0);
+        if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS){
+            adjustJointPos(Serial, 5, -adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_RIGHT)){
-            adjustJointPos(Serial, 5, 15.0);
+        if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS){
+            adjustJointPos(Serial, 6, adjustment);
         }
-        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_LEFT)){
-            adjustJointPos(Serial, 5, -15.0);
+        if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS){
+            adjustJointPos(Serial, 6, -adjustment);
         }
 
         glfwSwapBuffers(window); // Render the current frame
