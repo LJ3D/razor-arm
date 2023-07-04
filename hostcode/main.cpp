cd06a2cff8317fc6ea9cc07d9d39ce07ae2876f8
Lucian James <65134690+LJ3D@users.noreply.github.com>
2023-07-04 12:30:08 +0100

    home arm

--- hostcode/main.cpp
+++ hostcode/main.cpp
@@ -2,10 +2,15 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 #include "gl_debug.hpp"
+#include "arduinoserialio/arduinoSerial.hpp"
+#include <thread>
 
 #define DEBUG true
 
 int main(){
+    arduinoSerial Serial;
+    Serial.openPort("/dev/ttyACM0");
+    Serial.begin(B9600);
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -29,6 +34,18 @@ int main(){
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
+
+
+        if(GLFW_PRESS == glfwGetKey(window, GLFW_KEY_A)){
+            Serial.print("HOME");
+            char response[256] = {0};
+            // Wait a bit for the response
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            Serial.readBytes(response, 256);
+            std::cout << response << std::endl;
+        }
+
+
         glfwSwapBuffers(window);
     }
     glfwTerminate();
