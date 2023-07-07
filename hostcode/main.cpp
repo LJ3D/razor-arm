b9c508209049924987c147b1ca6dc019fdbeaec0
Lucian James <65134690+LJ3D@users.noreply.github.com>
2023-07-07 14:29:16 +0100

    wait time remove on l 84, set it to 1k fixed

--- hostcode/main.cpp
+++ hostcode/main.cpp
@@ -17,7 +17,7 @@
 #define SYNC_TIMEOUT 100
 
 int curr_speed = SPEED_START;
-int wait_time;
+int wait_time = 1000;
 /*
     Reads all the current joint positions from the arm.
     Currently only included as way to get potential debug info.
@@ -81,7 +81,6 @@ void setSpeed(arduinoSerial& Serial, int speed = SPEED_START, bool noset = false
     if(!noset){
         curr_speed = speed;
     }
-    wait_time = 10000 / speed;
     Serial.print("SPEED " + std::to_string(speed) + "\n");
     std::cout << "setSpeed(): Set speed to " << speed << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(SYNC_TIMEOUT));
